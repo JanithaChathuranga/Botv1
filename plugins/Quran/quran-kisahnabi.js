3898991cf1b1fe4c9b8bc6587de8ca024eb64b8e
Janiya <86745601+JanithaChathuranga@users.noreply.github.com>
2023-11-06 21:48:53 +0530

    Add files via upload

---
+++ plugins/Quran/quran-kisahnabi.js
@@ -0,0 +1,3 @@
+// Klo mau pake, pake aja ini bkn enc cma terser aja
+
+import fetch from"node-fetch";let handler=async(m,{text:text,usedPrefix:usedPrefix,command:command})=>{if(!text)throw`Masukan nama nabi\nExample: ${usedPrefix+command} adam`;let url=await fetch(`https://raw.githubusercontent.com/Aiinne/scrape/main/data/kisahnabi/${text}.json`),kisah=await url.json(),hasil=`Nabi : ${kisah.name}\nTanggal Lahir : ${kisah.thn_kelahiran}\nTempat Lahir : ${kisah.tmp}\nUsia : ${kisah.usia}\nKisah : ${kisah.description}`;conn.reply(m.chat,hasil,m)};handler.help=["kisahnabi <name>"],handler.tags=["quran"],handler.command=/^kisahnabi$/i;export default handler;
