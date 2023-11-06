aaeef44d2822b43a626eb71165cd51f24d9b62e2
Janiya <86745601+JanithaChathuranga@users.noreply.github.com>
2023-11-06 21:51:45 +0530

    Add files via upload

---
+++ plugins/Quotes/quote-fakta-quotes.js
@@ -0,0 +1,3 @@
+// Klo mau pake, pake aja ini bkn enc cma terser aja
+
+import fetch from"node-fetch";let handler=async(m,{conn:conn,command:command})=>{await fetch("https://raw.githubusercontent.com/HasamiAini/Bot_Takagisan/main/faktanya.txt").then((res=>res.text())).then((body=>{let splitnix=body.split("\n"),randomnix=splitnix[Math.floor(Math.random()*splitnix.length)];conn.sendButton(m.chat,randomnix,wm,[["NEXT",`/${command}`]],m)}))};handler.help=["fakta"],handler.tags=["quotes"],handler.command=/^(fakta|faktaunik)$/i,handler.fail=null;export default handler;
