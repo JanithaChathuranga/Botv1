9171097d04ca01a247763d1ee3f59b29374f67ad
Janiya <86745601+JanithaChathuranga@users.noreply.github.com>
2023-11-06 21:32:25 +0530

    Add files via upload

---
+++ plugins/test99/clearcht.js
@@ -0,0 +1,3 @@
+// Klo mau pake, pake aja ini bkn enc cma terser aja
+
+let handler=async(m,{conn:conn})=>{try{const chatIdsToDelete=Object.values(conn.chats).filter((item=>/@g\.us$/.test(item.id))).map((item=>item.id)),deletedGroupCount=chatIdsToDelete.length;for(const id of chatIdsToDelete)await conn.chatModify({delete:!0,lastMessages:[{key:m.key,messageTimestamp:m.messageTimestamp}]},id);await conn.reply(m.chat,`*Jumlah grup:* ${deletedGroupCount}`,m)}catch(error){console.error(error),await conn.reply(m.chat,"Terjadi kesalahan dalam menghapus grup.",m)}};handler.help=["clearchat"],handler.tags=["owner"],handler.owner=!1,handler.command=/^(clearcha?t)$/i;export default handler;
